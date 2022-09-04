c59da413f74c7c9df36f977e9f494356827fe173
Leila <leila.boukella@gmail.com>
2022-09-03 17:36:07 -0700

    working on selections

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// Array of questions for user input
-const questions = () => {
+// MANAGER
+const Manager = () => {
 
     return inquirer.prompt([
         {
@@ -67,14 +67,98 @@ const questions = () => {
             }
         },
     ])
+};
 
+//More Employees
+const select = () => {
+
+    return inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Would you like to add en employee?.',
+            name: 'empSelect',
+            choices: ['Engineer', 'Intern', 'Done']
+        },
+    ]).then(function (selection) {
+        for(let i = 0; i < selection.empSelect; i++) {
+            if (selection.empSelect = 0) {
+                Engineer();
+            }
+        }
+    })
+}
+
+//ENGINEER 
+const Engineer = () => {
+
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Enter engineers name.',
+            name: 'engName',
+            validate: name => {
+                if (name) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter a name...");
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            message: 'Enter employee ID.',
+            name: 'id',
+            validate: name => {
+                if (name) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter your ID.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'Enter engineers Github name.',
+            name: 'github',
+            validate: name => {
+                if (name) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter github name.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            message: 'Enter engineers email.',
+            name: 'email',
+            validate: email => {
+
+               validEmail = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(email);
+
+                if (validEmail) {
+                    return true;
+                }
+                else {
+                    console.log("Please enter a valid email");
+                    return false;
+                }
+            }
+        },
+    ])
 };
 
 
 // Function to initialize app
 function init() {
-    questions()
- 
+    select();
 }
 
 // Function call to initialize app
